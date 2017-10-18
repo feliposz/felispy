6ec9921c29ba8708584519f6bd32efcb1017d070
feliposz <feliposz@gmail.com>
2017-10-18 00:10:51 -0300

    Implemented REPL

--- src/main.c
+++ src/main.c
@@ -1,15 +1,72 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void hello(int n)
+#ifdef _WIN32
+#include <string.h>
+#include <windows.h>
+
+HANDLE hOutput;
+
+char* readline(char* prompt)
 {
-    while (n-- > 0)
+    static char buffer[2048];
+
+    SetConsoleTextAttribute(hOutput, 9);
+    fputs(prompt, stdout);
+
+    SetConsoleTextAttribute(hOutput, 7);
+    fgets(buffer, 2048, stdin);
+    char* result = (char*) malloc(strlen(buffer) + 1);
+    strcpy(result, buffer);
+    size_t last = strlen(result) - 1;
+    if (result[last] == '\n')
     {
-        puts("Hello world!");
+        result[last] = '\0';
     }
+    return result;
 }
 
-int main(int argc, char **argv)
+void add_history(char *unused)
 {
-    hello(5);
+}
+
+#else
+
+#include <editline/readline.h>
+#include <editline/history.h>
+
+#endif
+
+int main(int argc, char** argv)
+{
+
+#ifdef _WIN32
+    CONSOLE_SCREEN_BUFFER_INFO csbInfo;
+    hOutput = GetStdHandle(STD_OUTPUT_HANDLE);
+    GetConsoleScreenBufferInfo(hOutput, &csbInfo);
+    SetConsoleTextAttribute(hOutput, 10);
+#endif
+    puts("Felispy 0.0.1 - by Felipo");
+    puts("Type ctrl+C to exit.\n");
+    int counter = 0;
+
+    while (1)
+    {
+        char *input = readline("Felispy> ");
+#ifdef _WIN32
+        SetConsoleTextAttribute(hOutput, 15);
+#endif
+        printf("Result(%d): ", counter++);
+#ifdef _WIN32
+        SetConsoleTextAttribute(hOutput, 14);
+#endif
+        printf("%s\n", input);
+        free(input);
+    }
+
+#ifdef _WIN32
+    SetConsoleTextAttribute(hOutput, csbInfo.wAttributes);
+#endif
+
     return 0;
 }
