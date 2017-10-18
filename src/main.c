1feb5b05068fb1c27716aec284f9851a5f8cbba4
feliposz <feliposz@gmail.com>
2017-10-18 09:52:11 -0300

    Parsing expressions

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "mpc.h"
 
 #ifdef _WIN32
-#include <string.h>
 #include <windows.h>
 
 HANDLE hOutput;
@@ -39,6 +40,36 @@ void add_history(char *unused)
 
 int main(int argc, char** argv)
 {
+    mpc_parser_t* Number = mpc_new("number");
+    mpc_parser_t* Operator = mpc_new("operator");
+    mpc_parser_t* Expr = mpc_new("expr");
+    mpc_parser_t* Lispy = mpc_new("lispy");
+
+#if 0
+    char* Grammar =
+        "number   : /-?[0-9]+/ ;                            "
+        "operator : '+' | '-' | '*' | '/' ;                 "
+        "expr     : <number> | '(' <operator> <expr>+ ')' ; "
+        "lispy    : /^/ <operator> <expr>+ /$/ ;            ";
+#endif
+
+#if 0
+    char* Grammar =
+        "number   : /-?[0-9]+\\.?[0-9]*/ ; "
+        "operator : '+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\" | \"mod\" ; "
+        "expr     : <number> | '(' <operator> <expr>+ ')' ; "
+        "lispy    : /^/ <operator> <expr>+ /$/ ;  ";
+#endif
+
+#if 1
+    char* Grammar =
+        "number   : /-?[0-9]+\\.?[0-9]*/ ; "
+        "operator : '+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\" | \"mod\" ; "
+        "expr     : <number> <operator> <expr> | '(' <expr> ')' <operator> <expr> | '(' <expr> ')' | <number> ; "
+        "lispy    : /^/ <expr> /$/ ;  ";
+#endif
+
+    mpca_lang(MPCA_LANG_DEFAULT, Grammar, Number, Operator, Expr, Lispy);
 
 #ifdef _WIN32
     CONSOLE_SCREEN_BUFFER_INFO csbInfo;
@@ -48,18 +79,33 @@ int main(int argc, char** argv)
 #endif
     puts("Felispy 0.0.1 - by Felipo");
     puts("Type ctrl+C to exit.\n");
-    int counter = 0;
 
     while (1)
     {
-        char *input = readline("Felispy> ");
+        char* input = readline("Felispy> ");
+
+        mpc_result_t result;
+        if (mpc_parse("<stdin>", input, Lispy, &result))
+        {
 #ifdef _WIN32
-        SetConsoleTextAttribute(hOutput, 15);
+            SetConsoleTextAttribute(hOutput, 15);
 #endif
-        printf("Result(%d): ", counter++);
+            mpc_ast_print(result.output);
+            mpc_ast_delete(result.output);
+        }
+        else
+        {
+#ifdef _WIN32
+            SetConsoleTextAttribute(hOutput, 13);
+#endif
+            mpc_err_print(result.error);
+            mpc_err_delete(result.error);
+        }
+
 #ifdef _WIN32
         SetConsoleTextAttribute(hOutput, 14);
 #endif
+
         printf("%s\n", input);
         free(input);
     }
@@ -68,5 +114,6 @@ int main(int argc, char** argv)
     SetConsoleTextAttribute(hOutput, csbInfo.wAttributes);
 #endif
 
+    mpc_cleanup(4, Number, Operator, Expr, Lispy);
     return 0;
 }
