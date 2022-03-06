49c7c6b68a6effa7d2ad060026e14d31313a250d
feliposz <feliposz@gmail.com>
2022-03-05 21:10:12 -0300

    Evaluating expressions

--- src/main.c
+++ src/main.c
@@ -38,6 +38,134 @@ void add_history(char *unused)
 
 #endif
 
+int count_all(mpc_ast_t* node)
+{
+    if (node->children_num == 0)
+    {
+        return 1;
+    }
+    else if (node->children_num >= 1)
+    {
+        int total = 1;
+        for (int i = 0; i < node->children_num; i++)
+        {
+            total += count_all(node->children[i]);
+        }
+        return total;
+    }
+    return 0;
+}
+
+int count_leaves(mpc_ast_t *t)
+{
+    if (t->children_num == 0)
+    {
+        return 1;
+    }
+    int count = 0;
+    for (int i = 0; i < t->children_num; i++)
+    {
+        count += count_leaves(t->children[i]);
+    }
+    return count;
+}
+
+int max_leaves(mpc_ast_t *t)
+{
+    if (t->children_num == 0)
+    {
+        return 0;
+    }
+    int max = t->children_num;
+    for (int i = 0; i < t->children_num; i++)
+    {
+        int count = max_leaves(t->children[i]);
+        max = max < count ? count : max;
+    }
+    return max;
+}
+
+int count_nodes(mpc_ast_t *t)
+{
+    if (t->children_num == 0)
+    {
+        return 0;
+    }
+    int count = 1;
+    for (int i = 0; i < t->children_num; i++)
+    {
+        count += count_nodes(t->children[i]);
+    }
+    return count;
+}
+
+long eval_op(long x, char* op, long y)
+{
+    if (strcmp(op, "+") == 0)
+    {
+        return x + y;
+    }
+    else if (strcmp(op, "-") == 0)
+    {
+        return x - y;
+    }
+    else if (strcmp(op, "*") == 0)
+    {
+        return x * y;
+    }
+    else if (strcmp(op, "/") == 0)
+    {
+        return x / y;
+    }
+    else if (strcmp(op, "%") == 0)
+    {
+        return x % y;
+    }
+    else if (strcmp(op, "^") == 0)
+    {
+        long result = 1;
+        while (y-- > 0)
+        {
+            result *= x;
+        }
+        return result;
+    }
+    else if (strcmp(op, "min") == 0)
+    {
+        return x < y ? x : y;
+    }
+    else if (strcmp(op, "max") == 0)
+    {
+        return x > y ? x : y;
+    }
+    return 0;
+}
+
+long eval(mpc_ast_t* t)
+{
+    if (strstr(t->tag, "number"))
+    {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+    long result = eval(t->children[2]);
+    // unary plus / minus
+    if (t->children_num == 4 && strcmp(op, "-") == 0)
+    {
+        result = -result;
+    }
+    else
+    {
+        for (int i = 3; i < t->children_num - 1; i++)
+        {
+            result = eval_op(result, op, eval(t->children[i]));
+        }
+    }
+
+    return result;
+}
+
 int main(int argc, char** argv)
 {
     mpc_parser_t* Number = mpc_new("number");
@@ -45,12 +173,12 @@ int main(int argc, char** argv)
     mpc_parser_t* Expr = mpc_new("expr");
     mpc_parser_t* Lispy = mpc_new("lispy");
 
-#if 0
+#if 1
     char* Grammar =
-        "number   : /-?[0-9]+/ ;                            "
-        "operator : '+' | '-' | '*' | '/' ;                 "
+        "number   : /-?[0-9]+/ ; "
+        "operator : '+' | '-' | '*' | '/' | '%' | '^' | \"min\" | \"max\"; "
         "expr     : <number> | '(' <operator> <expr>+ ')' ; "
-        "lispy    : /^/ <operator> <expr>+ /$/ ;            ";
+        "lispy    : /^/ <operator> <expr>+ /$/ ; ";
 #endif
 
 #if 0
@@ -61,7 +189,7 @@ int main(int argc, char** argv)
         "lispy    : /^/ <operator> <expr>+ /$/ ;  ";
 #endif
 
-#if 1
+#if 0
     char* Grammar =
         "number   : /-?[0-9]+\\.?[0-9]*/ ; "
         "operator : '+' | '-' | '*' | '/' | '%' | \"add\" | \"sub\" | \"mul\" | \"div\" | \"mod\" ; "
@@ -84,13 +212,28 @@ int main(int argc, char** argv)
     {
         char* input = readline("Felispy> ");
 
+#ifdef _WIN32
+        SetConsoleTextAttribute(hOutput, 12);
+#endif
+        printf("Input: %s\n", input);
+
         mpc_result_t result;
         if (mpc_parse("<stdin>", input, Lispy, &result))
         {
+            printf("Count: %d\n", count_all(result.output));
+            printf("Nodes: %d\n", count_nodes(result.output));
+            printf("Leaves: %d\n", count_leaves(result.output));
+            printf("Max. Leaves: %d\n", max_leaves(result.output));
 #ifdef _WIN32
             SetConsoleTextAttribute(hOutput, 15);
 #endif
             mpc_ast_print(result.output);
+
+#ifdef _WIN32
+            SetConsoleTextAttribute(hOutput, 14);
+#endif
+            printf("%li\n", eval(result.output));
+
             mpc_ast_delete(result.output);
         }
         else
@@ -102,11 +245,6 @@ int main(int argc, char** argv)
             mpc_err_delete(result.error);
         }
 
-#ifdef _WIN32
-        SetConsoleTextAttribute(hOutput, 14);
-#endif
-
-        printf("%s\n", input);
         free(input);
     }
 
